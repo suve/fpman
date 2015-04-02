55c297fca1f159b5375d0c612055fc13539f5bdc
suve <veg@svgames.pl>
2015-04-02 20:45:18 +0200

    Added file caching

--- src/fpman.pas
+++ src/fpman.pas
@@ -10,8 +10,57 @@ Var
    InputLine, InputStr : AnsiString;
    Desc : TFunctionDesc;
    
+   StoredInCache:Boolean;
+   CacheDir : AnsiString;
+   
    TmpName : AnsiString;
    TmpFile : Text;
+
+Const
+   HomeVar = {$IFDEF   LINUX} 'HOME';    {$ELSE}
+             {$IFDEF WINDOWS} 'APPDATA'; {$ELSE}
+             {$FATAL HomeVar must be set.} {$ENDIF} {$ENDIF}
+             
+   ConfDir = {$IFDEF   LINUX} '/.suve/fpman/'; {$ELSE}
+             {$IFDEF WINDOWS} '\suve\fpman\';  {$ELSE}
+             {$FATAL ConfDir must be set.} {$ENDIF} {$ENDIF}
+
+Function GetConfPath():AnsiString;
+begin
+   Result := GetEnvironmentVariable(HomeVar) + ConfDir
+end;
+
+Function CreateCacheDir():Boolean;
+begin
+   If(Desc.Package_ <> '')
+      then CacheDir += LowerCase(Desc.Package_) + '/'
+      else CacheDir += 'unknown/';
+   
+   If(Desc.Unit_ <> '')
+      then CacheDir += LowerCase(Desc.Unit_) + '/'
+      else CacheDir += 'unknown/';
+   
+   If(Not ForceDirectories(GetConfPath() + CacheDir)) then begin
+      CacheDir := '~/.suve/fpman/' + CacheDir;
+      Exit(False)
+   end;
+   
+   TmpName := GetConfPath() + CacheDir + LowerCase(Desc.Name) + '.man';
+   Exit(True)
+end;
+
+Function OutputToFile():Boolean;
+begin
+   Assign(TmpFile, TmpName);
+   
+   {$I-} Rewrite(TmpFile); {$I+}
+   If(IOResult() <> 0) then Exit(False);
+   
+   OutputTroff(Desc, TmpFile);
+   Close(TmpFile);
+   
+   Exit(True)
+end;
 
 // fpman.main()
 begin
@@ -41,17 +90,23 @@ begin
    
    ParseFunctionHTML(InputStr, Desc);
    
-   TmpName := GetTempFileName('', 'fpman-');
-   Assign(TmpFile, TmpName);
+   If(Not CreateCacheDir()) then begin
+      Writeln(stderr, 'fpman: failed to create cache directory ', CacheDir);
+      StoredInCache := False
+   end else
+   If(Not OutputToFile()) then begin
+      Writeln(stderr, 'fpman: failed to write cache file ', CacheDir + LowerCase(Desc.Name) + '.man');
+      StoredInCache := False
+   end else
+      StoredInCache := True;
    
-   {$I-} Rewrite(TmpFile); {$I+}
-   If(IOResult() <> 0) then begin
-      Writeln(stderr, 'fpman: unable to write to temporary file: ',TmpName);
-      Halt(1)
+   If(Not StoredInCache) then begin
+      TmpName := GetTempFileName('', 'fpman-');
+      If(Not OutputToFile()) then begin
+         Writeln(stderr, 'fpman: failed to write temporary file ',TmpName);
+         Halt(1)
+      end
    end;
-   
-   OutputTroff(Desc, TmpFile);
-   Close(TmpFile);
    
    fpExecLP('man', [TmpName])
 end.
