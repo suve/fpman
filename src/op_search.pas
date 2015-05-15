3815bf7d7186aa5c53e2fd74c65cccb2ad1be545
suve <veg@svgames.pl>
2015-05-15 11:20:08 +0200

    Prompt user for page number when multiple matches found

--- src/op_search.pas
+++ src/op_search.pas
@@ -33,9 +33,11 @@ Var
    TmpName : AnsiString;
    DoQuit : Boolean;
    rset : TResultSet;
-   Pages : sInt;
+   Idx : sInt;
 begin
    rset.Create();
+   DoQuit := False;
+   
    If(Not db.FindPage(ModeArg, rset)) then begin
       Writeln(stderr, 'fpman: failed to search in database');
       DoQuit := True
@@ -49,8 +51,8 @@ begin
       If(rset.Count <= 0) then begin
          Writeln(stderr, 'fpman: no pages with similar names found');
       
-         If(Not NumberOfPages(Pages)) then Writeln(stderr, 'fpman: failed to check number of pages in database')
-         else If(Pages < 1) then Writeln(stderr, 'fpman: database seems to be empty, consider running fpman --import')
+         If(Not NumberOfPages(Idx)) then Writeln(stderr, 'fpman: failed to check number of pages in database')
+         else If(Idx < 1) then Writeln(stderr, 'fpman: database seems to be empty, consider running fpman --import')
       end else begin
          Writeln(stderr, 'fpman: ', rset.Count, ' similarly named pages found');
          print_rset(rset)
@@ -61,29 +63,34 @@ begin
    If(rset.Count > 1) then begin
       Writeln(stderr, 'fpman: multiple entries found for "', ModeArg, '":');
       print_rset(rset);
-      DoQuit := True
-   end else begin
+      
+      Writeln(stderr, 'fpman: which page do you want? (number, or 0 to exit)');
+      Read(TmpName);
+      
+      Idx := StrToIntDef(TmpName, 0) - 1;
+      If((Idx < 0) or (Idx > rset.Count)) then DoQuit := True
+   end else
+      Idx := 0; // Found exactly one page, set page index to 0
+   
+   // Page found in DB, check if file exists
+   If(Not DoQuit) then begin
       TmpName := 'pages/' + 
-         LowerCase(rset[0].Package_) + '/' +
-         LowerCase(rset[0].Unit_) + '/' +
-         LowerCase(rset[0].Page) + '.man'
+         LowerCase(rset[Idx].Package_) + '/' +
+         LowerCase(rset[Idx].Unit_) + '/' +
+         LowerCase(rset[Idx].Page) + '.man'
       ;
-     
      
       If(Not FileExists(GetConfPath() + TmpName)) then begin
          Writeln(stderr, 'fpman: file ~/.suve/fpman/', TmpName, ' not found or is not readable');
          Writeln(stderr, 'fpman: you may want to run fpman --revalidate');
          DoQuit := True
-      end else begin
-         TmpName := GetConfPath() + TmpName;
-         DoQuit := False
       end
    end;
    
    db.Quit();
    if(DoQuit) then Halt(1);
    
-   fpExecLP('man', [TmpName]);
+   fpExecLP('man', [GetConfPath() + TmpName]);
    
    Writeln(stderr, 'fpman: failed to execute man');
    Halt(1)
